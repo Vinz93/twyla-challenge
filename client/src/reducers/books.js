cd43375ce04d86c0c1eefdf1e9baed668260d0c4
Vincenzo Bianco <vincenzobianco1993@gmail.com>
2018-06-20 16:30:34 -0400

    add handler to book reducer

--- client/src/reducers/books.js
+++ client/src/reducers/books.js
@@ -1,9 +1,11 @@
-import { FETCH_BOOKS } from '../constants/ActionTypes';
+import { FETCH_BOOKS, CREATE_BOOK_SUCCESS } from '../constants/ActionTypes';
 
 export default function counter(state = [], action) {
   switch (action.type) {
     case FETCH_BOOKS:
       return [...action.response];
+    case CREATE_BOOK_SUCCESS:
+      return [...state, action.response];
     default:
       return state;
   }
