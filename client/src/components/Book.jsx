08c4877498ac89d980c0bc37c1703fd9d3d307de
Vincenzo Bianco <vincenzobianco1993@gmail.com>
2018-06-21 12:29:10 -0400

    solved bug on reviews after create a book

--- client/src/components/Book.jsx
+++ client/src/components/Book.jsx
@@ -3,29 +3,33 @@ import React from 'react';
 import BookModal from './BookModal';
 import ReviewForm from '../containers/ReviewForm';
 
-const Book = ({ book }) => (
-  <li className="book">
-    <p className="title">{book.title}</p>
-    <div className="book-info">
-      <p className="isbn"> ISBN: {book.isbn}</p>
-      <p className="isbn"> Added by: {book.addedBy}</p>
-    </div>
-    <div className="reviews-container">
-    <h3>Reviews</h3>
-    <BookModal buttonName={"Add review"}>
-      <ReviewForm bookId={book.id} />
-    </BookModal>
-      <ul>
-        {book.reviews.map(({ rate, comment, user, id }) => (
-          <li key={id} className="comment">
-            <span>{user.userName}</span>
-            <span> {rate}/5</span>
-            <p>{`"${comment}"`}</p>
-          </li>
-          ))}
-      </ul>
-    </div>
-  </li>
+const Book = ({ book }) => {
+  const reviews = book.reviews || [];
+  return (
+    <li className="book">
+      <p className="title">{book.title}</p>
+      <div className="book-info">
+        <p className="isbn"> ISBN: {book.isbn}</p>
+        <p className="isbn"> Added by: {book.addedBy}</p>
+      </div>
+      <div className="reviews-container">
+      <h3>Reviews</h3>
+      <BookModal buttonName={"Add review"}>
+        <ReviewForm bookId={book.id} />
+      </BookModal>
+        <ul>
+          {
+            reviews.map(({ rate, comment, user, id }) => (
+            <li key={id} className="comment">
+              <span>{user.userName}</span>
+              <span> {rate}/5</span>
+              <p>{`"${comment}"`}</p>
+            </li>
+            ))}
+        </ul>
+      </div>
+    </li>
 );
+};
 
 export default Book;
--- client/src/components/BookList.jsx
+++ client/src/components/BookList.jsx
@@ -2,10 +2,18 @@ import React from 'react';
 
 import Book from './Book';
 
-const BookList = ({ books }) => (
-  <ul className="book-list">
+const BookList = ({ books }) => {
+  if (books.lenght === 0) {
+    return (
+      <p>No books added yet!</p>
+    );
+  } else {
+    return (
+    <ul className="book-list">
     {books.map(book => <Book key={book.id} book={book}/>)}
-  </ul>
-);
+  </ul>);
+  }
+};
+
 
 export default BookList;
