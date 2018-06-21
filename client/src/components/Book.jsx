e4edbe6c88e6e5c3f50a4296987fbd037be14673
Vincenzo Bianco <vincenzobianco1993@gmail.com>
2018-06-21 08:08:17 -0400

    add review form

--- client/src/components/Book.jsx
+++ client/src/components/Book.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
+import BookModal from './BookModal';
+import ReviewForm from '../containers/ReviewForm';
+
 const Book = ({ book }) => (
   <li className="book">
     <p className="title">{book.title}</p>
     <p className="isbn"> ISBN: {book.isbn}</p>
+    <BookModal buttonName={"Add review"}>
+      <ReviewForm />
+    </BookModal>
   </li>
 );
 
---
+++ client/src/containers/ReviewForm.jsx
@@ -0,0 +1,51 @@
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+
+import { reviewBook } from '../actions';
+
+class ReviewForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { rate: 1, comment: '' };
+    this.handleRate = this.handleRate.bind(this);
+    this.handleComment = this.handleComment.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleRate(event) {
+    this.setState({ rate: event.target.value });
+  }
+  handleComment(event) {
+    this.setState({ comment: event.target.value });
+  }
+  handleSubmit(event) {
+    const { rate, comment } = this.state;
+    this.props.reviewBook({
+      rate,
+      comment
+    });
+    event.preventDefault();
+  }
+  render() {
+    return (
+      <div className="review-form">
+        <h2>Review</h2>
+        <form onSubmit={this.handleSubmit}>
+        <label className="input-group">
+          <span className="input-label">Rate</span>
+          <input type="number" value={this.state.rate} onChange={this.handleRate}/>
+        </label>
+        <label className="input-group">
+          <span className="input-label">comment</span>
+          <input type="text" value={this.state.comment} onChange={this.handleComment}/>
+        </label>
+        <input className="send" type="submit" value="Submit" />
+        </form>
+      </div>
+    );
+  }
+}
+
+ReviewForm.propTypes = {
+  reviewBook: PropTypes.func.isRequired,
+};
+export default connect(null, { reviewBook })(ReviewForm);
